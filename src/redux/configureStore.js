e5530798c2fdecbc928d9a689fce018910f4747f
alexmedinasf <alexsales001@hotmail.com>
2022-11-18 12:34:53 -0300

    configureStore now get reducer and preloaded data

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,17 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
 import categoryReducer from './categories/categories';
+import initialData from './initialData';
 
 const rootReducer = combineReducers({
-  bookReducer,
-  categoryReducer,
-});
-
-const store = configureStore(rootReducer);
-
-export default store;
+    books: bookReducer,
+    categories: categoryReducer,
+  });
+  
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState: initialData,
+  });
+  
+  export default store;
+  
