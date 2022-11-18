807f039282878794ddcf35cdf6240abc0bdaf00f
alexmedinasf <alexsales001@hotmail.com>
2022-11-18 12:33:46 -0300

    Linked redux to booklist

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,38 +1,24 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
+import { removeBook } from '../redux/books/books';
 
 const BookList = () => {
-  const dummyData = [
-    {
-      id: 1,
-      title: 'Grande Sertão Veredas',
-      author: 'Guimarães Rose',
-      genre: 'Novel',
-    },
-    {
-      id: 2,
-      title: 'Green Mile',
-      author: 'Stephen King',
-      genre: 'Drama',
-    },
-    {
-      id: 3,
-      title: 'Tender is the flesh',
-      author: 'Agustina Bazterrica',
-      genre: 'Sci-fiction',
-    },
-  ];
-
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const handleRemove = (book) => {
+    dispatch(removeBook(book));
+  };
   return (
     <ul className="bookList">
-      {dummyData.map((book) => (
+      {books.map((book) => (
         <li key={book.id}>
           <div className="bookWrapper">
             <div className="genre">{book.genre}</div>
             <Book title={book.title} author={book.author} />
             <div className="bookActions">
               <button className="comments" type="button" id={`comments-${book.id}`}>Comments</button>
-              <button className="remove" type="button" id={`remove-${book.id}`}>Remove</button>
+              <button className="remove" type="button" id={`remove-${book.id}`} onClick={() => handleRemove(book)}>Remove</button>
               <button className="edit" type="button" id={`edit-${book.id}`}>Edit</button>
             </div>
           </div>
