6deb75309713547427a699cf60bd04a296a7dffb
alexmedinasf <alexsales001@hotmail.com>
2022-11-19 08:56:08 -0300

    Created api info

---
+++ src/utils/apiInfo.js
@@ -0,0 +1,5 @@
+const API_ID = 'c47ee557243941f3893aa4274c202932';
+const API_BASE = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+const API_PATH = `${API_BASE}/apps/${API_ID}/books`;
+
+export { API_ID, API_BASE, API_PATH };
